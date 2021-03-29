a02eff56407b0bdc22aa46b71a4e395bf527315f
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-03-28 21:28:56 -0500

    Most Constrained implemented

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,4 +1,4 @@
-#Include logic for reading command line, files, and main processes here.
+#Global vars
 domain = {
     'X': [0, 1, 2],
     'Y': [0, 1, 2],
@@ -9,16 +9,32 @@ chosenVars = []
 
 #Functions
 def backtrack():
-    print("Backtracking")
+    print(mostConstrained())
 
 def forwardCheck():
     print("ForwardChecking")
 
 def mostConstrained():
-    return "Var"
+    mostConList = []
+    smallestDomain = 99
+    #Find smallest domains
+    for var in domain:
+        if len(domain[var]) < smallestDomain:
+            mostConList = [var]
+            smallestDomain = len(domain[var])
+        elif len(domain[var]) == smallestDomain:
+            mostConList.append(var)
+    #Check if only one value, otherwise use most constraining
+    if len(mostConList) == 1:
+        return mostConList[0]
+    else:
+        return mostConstraining(mostConList)
 
 def mostConstraining(valList):
     return "Var"
 
 def alphabetical(valLists):
     return "Var"
+
+#Include logic for reading command line, files, and main processes here.
+backtrack()
