b7071dcf7b9093de662fadd4a044925a86ef3a99
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-04-06 18:12:52 -0500

    Basis of Resolution complete

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -21,6 +21,43 @@ def negateClause(clause):
         CNF.append([not clause[1][i]])
         CNF.append([])
         KB.append(CNF)
+
+def findNewClause():
+    for clausei in reversed(range(len(KB))):
+        newClauseFound = False
+        for clausej in range(clausei):
+            for x in range(len(KB[clausej][0])):
+                for y in range(len(KB[clausei][0])):
+                    if KB[clausei][0][y] == KB[clausej][0][x] and KB[clausei][1][y] != KB[clausej][1][x]:
+                        #print(KB[clausei]," and ",KB[clausej]," can cancel.")
+                        if len(KB[clausei][0]) == len(KB[clausej][0]):
+                            printKB()
+                            print("Contradiction {" + str(clausei + 1) + ", " + str(clausej + 1) + "}")
+                            return True #Contradiction found
+                        createNewClause(clausei, clausej, y, x)
+                        return findNewClause()
+                        newClauseFound = True
+                        break
+    return False #No contradiction found
+                        
+def createNewClause(clausei, clausej, commonLiterali, commonLiteralj):
+    newClause = []
+    literals = []
+    negations = []
+    for i in range(len(KB[clausei][0])):
+        if KB[clausei][0][i] != KB[clausei][0][commonLiterali]: #STILL NEED TO CHECK FOR DUPLICATES
+            literals.append(KB[clausei][0][i])
+            negations.append(KB[clausei][1][i])
+    for i in range(len(KB[clausej][0])):
+        if KB[clausej][0][i] != KB[clausej][0][commonLiteralj]: #STILL NEED TO CHECK FOR DUPLICATES
+            literals.append(KB[clausej][0][i])
+            negations.append(KB[clausej][1][i])
+
+    newClause.append(literals)
+    newClause.append(negations)
+    newClause.append([clausei + 1, clausej + 1])
+    KB.append(newClause)
+
 #Attempt to open files, return true if successful
 def openFiles():
     #Check if the given inFile file exists
@@ -93,7 +130,7 @@ if not inFile == None:
         CNF.append(literals)
         CNF.append(negations)
         CNF.append([])
-        print(CNF)
+        #print(CNF)
 
         nextLine = inFile.readline()
 
@@ -104,5 +141,8 @@ if not inFile == None:
             origClause = CNF
 
 negateClause(origClause)
-printKB()
+if findNewClause():
+    print("Valid")
+else:
+    print("Not Valid")
 closeFiles()
