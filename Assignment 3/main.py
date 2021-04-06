239087c198ed784183a9e8b443dbabb51f9e240c
Cameron <cameron.meyer02@gmail.com>
2021-04-05 20:36:44 -0500

    Sample Files + Read File Input and Initialize KB

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,14 +1,89 @@
 # Andrew Sylvester and Cameron Meyer
-# CS 4365 - Assignment 2 part 2
+# CS 4365 - Assignment 3 part 1
 # main.py
 
 import sys
 import os.path
 import re
+import array
 
 #Global vars
-
+inFile = None
+KB = [] #A 3D array of the knowledge base. Access in the form KB[Clause][Var/Val][Literal]
+        #There are n-1 clauses, 2 var/val options, and ?? number of literals
+origClause = None
 
 #Functions
+#Attempt to open files, return true if successful
+def openFiles():
+    #Check if the given inFile file exists
+    if(os.path.isfile(sys.argv[1])):
+        global inFile
+        inFile = open(sys.argv[1], "r")
+    else:
+        print('inFile file does not exist.')
+        return False
+
+    #The files were successfully opened
+    return True
+
+def closeFiles():
+    #Close files as needed
+    if(not inFile == None):
+        inFile.close()
+
+def printKB():
+    global KB
+    for clause in KB:
+        output = ""
+        for i in range(len(clause[0])):
+            if clause[1][i] == True:
+                output += "~"
+            output += clause[0][i]
+
+            if i < len(clause[0]) - 1:
+                output += " "
+        print(output)
 
 #Include logic for reading command line, files, and main processes here.
+#Check for exact number of command line arguments
+if not len(sys.argv) == 2:
+    print('Invalid number of arguments. Searching for 1 arguments and found ', len(sys.argv) - 1, '.') #Looking for 1 arg besides the python filename
+    exit()
+
+if not openFiles():   
+    exit()
+
+#Check if the inFile file is open
+if not inFile == None:
+    #Read in each line of data to an array and remove unnecessary characters
+    nextLine = inFile.readline()
+    while(not nextLine == ""):
+        literals = re.split("[ \n]", nextLine)
+        emptyCount = literals.count('')
+        for i in range(emptyCount):
+            literals.remove('')
+
+        negations = []
+
+        for i in range(len(literals)):
+            if literals[i].find("~") == 0:
+                negations.append(True)
+                literals[i] = literals[i][1:]
+            else:
+                negations.append(False)
+
+        CNF = []
+        CNF.append(literals)
+        CNF.append(negations)
+
+        nextLine = inFile.readline()
+
+        if not nextLine == "":
+            KB.append(CNF)
+        else:
+            #Final line is to be checked with resolution somehow, I don't think it goes in the KB??
+            origClause = CNF
+        
+printKB()
+closeFiles()
